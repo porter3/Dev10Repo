4b675e8b7a848ee2f6961de7d8bd46334a38fde3
Jake Porter <jakeporter310@gmail.com>
2019-10-18 15:46:52 -0400

    completed 1st iteration of JS

--- luckySevens.js
+++ luckySevens.js
@@ -18,11 +18,43 @@ function runGame(){
         return false;
     }
 
+    document.getElementById("startingBetResult").innerHTML = money;
+
+    var sidesOfDie = 6;
+    var rollCounter = 0;
+    var highScoreOfMoney = money;
+    var highestAmountRollCount = 0;
+
     //begin game loop
     do{
+        //roll dice
+        var dieOne = (Math.floor(Math.random() * sidesOfDie) + 1);
+        var dieTwo = (Math.floor(Math.random() * sidesOfDie) + 1);
+        //if dice equal 7, add 4 to money
+        if (dieOne + dieTwo == 7){
+            money += 4;
+        }
+        //else, subtract 1 from money
+        else{
+            money -= 1;
+        }
+        //increment roll counter
+        rollCounter += 1;
 
+        //check if current money is highest amount as of yet, assign high score to it if it is
+        if (money > highScoreOfMoney){
+            highScoreOfMoney = money;
+            highestAmountRollCount = rollCounter;
+        }
 
+        console.log("1st die: " + dieOne + " " + "2nd die: " + dieTwo + " " + 
+        "current money: " + money + " " + "rolls so far: " + " " + rollCounter);
     }
-    //check that there's money to play with, gane will end if there's not
+    //check that there's money to play with, game will end if there's not
     while (money > 0);
+
+    document.getElementById("totalRolls").innerHTML = rollCounter;
+    document.getElementById("highestAmountWon").innerHTML = highScoreOfMoney;
+    document.getElementById("highestAmountRollCount").innerHTML = highestAmountRollCount;
+
 }
